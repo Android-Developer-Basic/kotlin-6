3281d550236dae5991224f6237db1d32d17a639c
Andrusov <paandrusov@gid.ru>
2024-09-25 19:15:14 +0300

    Выполнено ДЗ Kotlin-6

--- src/main/kotlin/ru/otus/homework/Builder.kt
+++ src/main/kotlin/ru/otus/homework/Builder.kt
@@ -1,38 +1,52 @@
 package ru.otus.homework
 
 class Product {
+
     var part1: String? = null
     var part2: String? = null
     var part3: String? = null
-}
 
-class Builder {
-    private val product = Product()
+    class ProductBuilder {
+        private var part1: String? = null
+        private var part2: String? = null
+        private var part3: String? = null
 
-    fun setPart1(part1: String): Builder {
-        product.part1 = part1
-        return this
+        fun part1(part1: String): ProductBuilder {
+            this.part1 = part1
+            return this
+        }
+
+        fun part2(part2: String): ProductBuilder {
+            this.part2 = part2
+            return this
+        }
+
+        fun part3(part3: String): ProductBuilder {
+            this.part3 = part3
+            return this
+        }
+
+        fun build(): Product {
+            val result = Product()
+
+            result.part1 = part1
+            result.part2 = part2
+            result.part3 = part3
+
+            return result
+        }
     }
 
-    fun setPart2(part2: String): Builder {
-        product.part2 = part2
-        return this
-    }
-
-    fun setPart3(part3: String): Builder {
-        product.part3 = part3
-        return this
-    }
-
-    fun build(): Product {
-        return product
+    companion object {
+        fun builder(): ProductBuilder = ProductBuilder()
     }
 }
 
 fun main() {
-    val product = Builder().setPart1("Часть 1")
-        .setPart2("Часть 2")
-        .setPart3("Часть 3")
+    val product = Product.builder()
+        .part1("Часть 1")
+        .part2("Часть 2")
+        .part3("Часть 3")
         .build()
 
     println(product.part1)
