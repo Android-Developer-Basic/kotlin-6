bd5783b1c363608c68e8be2a8e7698340af9ae98
Evgenii Balandin <e.balandin@dt1520.ru>
2024-10-26 03:28:17 +0300

    Singleton pattern

--- src/main/kotlin/ru/otus/homework/patterns/singleton.kt
+++ src/main/kotlin/ru/otus/homework/patterns/singleton.kt
@@ -1,5 +1,7 @@
 package ru.otus.homework.patterns
 
+import java.util.TreeSet
+
 fun main() {
     println("Hello, singleton!")
     println("First call:")
@@ -10,6 +12,61 @@ fun main() {
     println(db2.data)
 
     println("db1 === db2: ${db1 === db2}")
+
+
+    println()
+    println(MemoryMonitor.getStackMemoryInfo(1024))  // Предполагаем 1 МБ на поток -Xss1m.
+    println()
+
+    val initialCapacity = 5_000_000
+
+    println("-=Array=-")
+    MemoryMonitor.collectGarbage()
+    println(MemoryMonitor.getHeapMemoryInfo())
+    println()
+    val array = IntArray(initialCapacity)
+    for(i: Int in 0.. initialCapacity - 1) {
+        array[i] = (i+1)*10
+    }
+    println(MemoryMonitor.getMemoryUsageDifferenceInfo())
+    println()
+
+
+    println("-=Set=-")
+    MemoryMonitor.collectGarbage()
+    println(MemoryMonitor.getHeapMemoryInfo())
+    println()
+    val set = HashSet<Int>(initialCapacity)
+    for(i: Int in 1..initialCapacity) {
+        set.add(i*10)
+    }
+    println(MemoryMonitor.getMemoryUsageDifferenceInfo())
+    println()
+
+
+    println("-=HashMap=-")
+    MemoryMonitor.collectGarbage()
+    println(MemoryMonitor.getHeapMemoryInfo())
+    println()
+    val hashMap = HashMap<Int, Int>(initialCapacity)
+    for(i: Int in 1..initialCapacity) {
+        hashMap[i] = i*10
+    }
+    println(MemoryMonitor.getMemoryUsageDifferenceInfo())
+    println()
+
+    println("-=TreeMap=-")
+    MemoryMonitor.collectGarbage()
+    println(MemoryMonitor.getHeapMemoryInfo())
+    println()
+
+    val treeMap = HashMap<Int, Int>(initialCapacity)
+    for(i: Int in 1..initialCapacity) {
+        treeMap[i] = i*10
+    }
+    println(MemoryMonitor.getMemoryUsageDifferenceInfo())
+    println()
+
 }
 
 class Database private constructor(val data: Map<String, String>) {
@@ -29,3 +86,50 @@ class Database private constructor(val data: Map<String, String>) {
         }
     }
 }
+
+object MemoryMonitor {
+    private val runtime: Runtime by lazy { Runtime.getRuntime() }
+
+    // Информация о памяти кучи
+    fun getHeapMemoryInfo(): String {
+        val maxMemory = runtime.maxMemory() / 1024 / 1024  // MB
+        val allocatedMemory = runtime.totalMemory() / 1024 / 1024  // MB
+        val freeMemory = runtime.freeMemory() / 1024 / 1024  // MB
+        return """
+            Информация о памяти кучи:
+            Максимальная память: $maxMemory МБ
+            Выделенная память: $allocatedMemory МБ
+            Свободная память: $freeMemory МБ
+            Используемая память: ${allocatedMemory - freeMemory} МБ
+        """.trimIndent()
+    }
+
+    // Информация о памяти стека с отложенной загрузкой, предполагая размер стека на поток
+    fun getStackMemoryInfo(assumedStackSizeKB: Int = 1024): String {
+        val activeThreads by lazy { Thread.activeCount() }
+        val estimatedStackUsageKB by lazy { activeThreads * assumedStackSizeKB }
+        return """
+            Информация о памяти стека:
+            Предполагаемый размер стека (на поток): ${assumedStackSizeKB} КБ
+            Активные потоки: $activeThreads
+            Оценка общего использования памяти стека: $estimatedStackUsageKB КБ
+        """.trimIndent()
+    }
+    // Собрать мусор
+    fun collectGarbage() {
+        runtime.gc()
+    }
+    // Вывести разницу в использовании памяти
+    fun getMemoryUsageDifferenceInfo(): String {
+        val beforeGC = runtime.totalMemory() - runtime.freeMemory()  // Использованная память до GC
+        collectGarbage()
+        val afterGC = runtime.totalMemory() - runtime.freeMemory()   // Использованная память после GC
+        val difference = (beforeGC - afterGC) / 1024 / 1024          // Разница в MB
+        return """
+        Использование памяти до сборки мусора: ${beforeGC / 1024 / 1024} МБ
+        Использование памяти после сборки мусора: ${afterGC / 1024 / 1024} МБ
+        Освобожденная память после сборки мусора: $difference МБ
+        """.trimIndent()
+    }
+}
+
