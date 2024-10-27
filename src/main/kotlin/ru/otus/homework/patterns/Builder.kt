1b3f5fde6fd428e447e2d01eed03d0150d736369
SpiritualAdviser <pomaxam@narod.ru>
2024-10-27 19:45:33 +0300

    add Builder

--- src/main/kotlin/ru/otus/homework/patterns/Builder.kt
+++ src/main/kotlin/ru/otus/homework/patterns/Builder.kt
@@ -1,2 +1,48 @@
 package ru.otus.homework.patterns
 
+fun main() {
+    val soupCoked = SoupBuilder()
+        .addWater()
+        .addSpices()
+        .addPotato()
+        .build()
+    println(soupCoked.soupComponents.toString())
+}
+
+interface CookSoup {
+    fun addWater(): SoupBuilder
+    fun addSpices(): SoupBuilder
+    fun addPotato(): SoupBuilder
+    fun build(): Soup
+}
+
+class SoupBuilder : CookSoup {
+    private val soup = Soup()
+
+    override fun addWater(): SoupBuilder {
+        soup.addComponent("water")
+        return this
+    }
+
+    override fun addSpices(): SoupBuilder {
+        soup.addComponent("spices")
+        return this
+    }
+
+    override fun addPotato(): SoupBuilder {
+        soup.addComponent("potato")
+        return this
+    }
+
+    override fun build(): Soup {
+        return soup
+    }
+}
+
+class Soup {
+    val soupComponents: MutableList<String> = mutableListOf()
+
+    fun addComponent(component: String) {
+        soupComponents.add(component)
+    }
+}
