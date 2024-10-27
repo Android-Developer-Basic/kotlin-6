1b8ee786587f6c858da8d077cd3763764b38bd61
SpiritualAdviser <pomaxam@narod.ru>
2024-10-27 18:10:07 +0300

    add Decorator

--- src/main/kotlin/ru/otus/homework/patterns/decorator.kt
+++ src/main/kotlin/ru/otus/homework/patterns/decorator.kt
@@ -1,2 +1,34 @@
 package ru.otus.homework.patterns
 
+fun main() {
+    createCar()
+}
+
+fun createCar() {
+    val poloCar = PoloCar()
+    println(poloCar)
+
+    val decoratedPolo = RedCollor(poloCar).decorate()
+    println(decoratedPolo)
+}
+
+interface Car {
+    fun decorate(): String
+}
+
+class PoloCar() : Car {
+    override fun decorate(): String {
+        return "polo"
+    }
+}
+
+class RedCollor(private val car: Car) : Car by car {
+    override fun decorate(): String {
+        return car.decorate() + paintRed()
+    }
+
+    private fun paintRed(): String {
+        return "Red"
+    }
+}
+
