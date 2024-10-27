605755f635f1ac4a3a75dc5ae321bc1b4c769e01
SpiritualAdviser <pomaxam@narod.ru>
2024-10-27 18:38:40 +0300

    add Command

--- src/main/kotlin/ru/otus/homework/patterns/Command.kt
+++ src/main/kotlin/ru/otus/homework/patterns/Command.kt
@@ -1,2 +1,43 @@
 package ru.otus.homework.patterns
 
+fun main() {
+    val lamp = Lamp()
+    val lightOn = LightTurnOn(lamp)
+    val lightOff = LightTurnOff(lamp)
+
+    RemoteControll(lightOn).pressButton()
+    RemoteControll(lightOff).pressButton()
+}
+
+interface Command {
+    fun execute()
+}
+
+class LightTurnOn(private val lite: Lamp) : Command {
+    override fun execute() {
+        lite.turnOn()
+    }
+}
+
+class LightTurnOff(private val lite: Lamp) : Command {
+    override fun execute() {
+        lite.turnOff()
+    }
+}
+
+class RemoteControll(private val comand: Command) {
+
+    fun pressButton() {
+        comand.execute()
+    }
+}
+
+class Lamp() {
+    fun turnOn() {
+        println("lightOn")
+    }
+
+    fun turnOff() {
+        println("lightOff")
+    }
+}
