fcf80ea7029d175032e7026026f7d179325c4a71
Валерий Асланов <valeryanatolievuch@gmail.com>
2024-11-12 23:22:54 +0500

    Изменена реализация Singleton в Database на ленивое свойство `instance` в companion object для устранения ошибок и улучшения потокобезопасности.

--- src/main/kotlin/ru/otus/homework/patterns/singleton.kt
+++ src/main/kotlin/ru/otus/homework/patterns/singleton.kt
@@ -1,46 +1,26 @@
 package ru.otus.homework.patterns
 
 fun main() {
-    println("Hello, singleton!")
-    println("First call:")
-    val db1 = Database.getInstance()
+    println("Первый вызов:")
+    val db1 = Database.instance
     println(db1.data)
-    println("Second call:")
-    val db2 = Database.getInstance()
+
+    println("Второй вызов:")
+    val db2 = Database.instance
     println(db2.data)
 
     println("db1 === db2: ${db1 === db2}")
-
-    println("Первый вызов ленивого Singleton:")
-    val lazyDb1 = LazyDatabase.instance
-    println(lazyDb1.data)
-    println("Второй вызов ленивого Singleton:")
-    val lazyDb2 = LazyDatabase.instance
-    println(lazyDb2.data)
-    println("lazyDb1 === lazyDb2: ${lazyDb1 === lazyDb2}")
 }
 
 class Database private constructor(val data: Map<String, String>) {
     companion object {
-        private var instance: Database? = null
-
-        fun getInstance(): Database {
-            if (instance == null) {
-                println("Initializing database...")
-                instance = Database(mapOf(
-                    "1" to "One",
-                    "2" to "Two",
-                    "3" to "Three"
-                ))
-            }
-            return instance!!
+        val instance: Database by lazy {
+            println("Инициализация ленивого Singleton для Database...")
+            Database(mapOf(
+                "1" to "One",
+                "2" to "Two",
+                "3" to "Three"
+            ))
         }
-    }
-}
-
-object LazyDatabase {
-    val instance: Database by lazy {
-        println("Инициализация ленивого Singleton...")
-        Database(mapOf("1" to "One", "2" to "Two", "3" to "Three"))
     }
 }
